05646a9167ac00da002c03bdeead9579a640fc6b
Björn <noir3698@yahoo.de>
2022-02-19 22:49:39 +0100

    File chooser and field validation of APIKey and selected db path

--- src/main/java/de/darkestnoir/bcm/WelcomeController.java
+++ src/main/java/de/darkestnoir/bcm/WelcomeController.java
@@ -1,21 +1,105 @@
 package de.darkestnoir.bcm;
 
+import java.io.File;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
 public class WelcomeController {
 
-	public void databaseSaveLocationClick() {
+	String databaseSaveFolderDefault1 = ((System.getenv("USERPROFILE")) + ("\\Documents\\") + ("BCM\\"));
+	String databaseSaveFolderDefault2 = ((System.getenv("USERPROFILE")) + ("\\Documents\\"));
+	String databaseSaveLocationDefault = ((System.getenv("USERPROFILE")) + ("\\Documents\\") + ("BCM\\") + ("database.db"));
 
-	}
+	@FXML
+	Stage WelcomeUIStage;
 
+	@FXML
 	public void rebrickableLinkClick() {
 
 	}
 
-	public void welcomeOKClick() {
+	@FXML
+	public void welcomeOKClick(ActionEvent event) {
+		String rebrickableApiKey = rebrickableApiKeyText.getText();
+		String databaseSaveLocation = databaseSaveLocationText.getText();
+		System.out.println(rebrickableApiKey); // temporary
+		System.out.println(databaseSaveLocation); // temporary
+	}
+
+	@FXML
+	private Button welcomeQuitButton;
+
+	@FXML
+	public void welcomeQuitClick(ActionEvent event) {
+
+		Stage WelcomeUIStage = (Stage) welcomeQuitButton.getScene().getWindow();
+		WelcomeUIStage.close();
 
 	}
 
-	public void welcomeQuitClick() {
+	@FXML
+	private Button databaseSaveLocationButton;
+
+	@FXML
+	public void databaseSaveLocationClick(ActionEvent event) {
+		FileChooser databaseLocationChooser = new FileChooser();
+		File databaseFolder = new File(databaseSaveFolderDefault1);
+		if (databaseFolder.exists() && databaseFolder.isDirectory()) {
+			databaseLocationChooser.setInitialDirectory(new File(databaseSaveFolderDefault1));
+		} else {
+			databaseLocationChooser.setInitialDirectory(new File(databaseSaveFolderDefault2));
+		}
+		databaseLocationChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Database Files", "*.db"));
+		databaseLocationChooser.setInitialFileName("database.db");
+		File databaseSaveLocation = databaseLocationChooser.showSaveDialog(WelcomeUIStage);
+		if (databaseSaveLocation != null) {
+			databaseSaveLocationText.setText(databaseSaveLocation.getAbsolutePath());
+		}
+	}
+
+	@FXML
+	private TextField databaseSaveLocationText;
+
+	@FXML
+	private Button welcomeOKButton;
+
+	@FXML
+	private TextField rebrickableApiKeyText;
+
+	@FXML
+	public void initialize() {
+
+		databaseSaveLocationText.setText(databaseSaveLocationDefault);
+		databaseSaveLocationText.textProperty().addListener(new ChangeListener<String>() {
+
+			@Override
+			public void changed(ObservableValue<? extends String> observable, String databaseSaveLocationTextOld, String databaseSaveLocationTextNew) {
+				checkOK(databaseSaveLocationTextNew, rebrickableApiKeyText.getText());
+			}
+		});
+
+		rebrickableApiKeyText.textProperty().addListener(new ChangeListener<String>() {
+
+			@Override
+			public void changed(ObservableValue<? extends String> observable, String rebrickableApiKeyTextOld, String rebrickableApiKeyTextNew) {
+				checkOK(databaseSaveLocationText.getText(), rebrickableApiKeyTextNew);
+			}
+		});
 
 	}
 
+	public void checkOK(String databaseSaveLocationTextNew, String rebrickableApiKeyTextNew) {
+		if (databaseSaveLocationTextNew.endsWith(".db") && rebrickableApiKeyTextNew.matches("^[A-Za-z0-9]{32,32}$")) {
+			welcomeOKButton.setDisable(false);
+		} else {
+			welcomeOKButton.setDisable(true);
+		}
+	}
 }
